daad9ad3c7ef20d282eaaf9784ffd74d5b70b2c0
marcos <marcos@abd22e6b-81ca-4246-bfda-0ddccaaa1310>
2018-03-21 00:23:33 +0000

    git-svn-id: http://10.18.0.71/svn/relator@24 abd22e6b-81ca-4246-bfda-0ddccaaa1310

--- controllers/Relatorios.php
+++ controllers/Relatorios.php
@@ -61,18 +61,18 @@ class Relatorios extends Controller {
     }
 
     function gerar($datasource, $nomeRelatorio) {
-        $_SESSION["FLUXO"][] = "Relatorios->gerar($datasource, $nomeRelatorio)";
+        //$_SESSION["FLUXO"][] = "Relatorios->gerar($datasource, $nomeRelatorio)";
         $relatorio = $this->relatorio->selectByEquals("nome", $nomeRelatorio);
         $parametros = count($_GET);
         if($relatorio[0]->parametrizado && !$parametros){
             $_SESSION['action'] = $this->router->link("Relatorios->gerar($datasource,$nomeRelatorio)"); //$router
-            $_SESSION['tela'] = true;
-            $_SESSION['toRelatorio'] = true;
+//            $_SESSION['tela'] = true;
+//            $_SESSION['toRelatorio'] = true;
             $rt = new RelatorioTela();
             $telaId = $rt->getBy(array("relatorio_id" => $relatorio[0]->id))->formulario_id;
             $formulario = new Formulario();
             $f = $formulario->getBy(array("id"=>$telaId));
-            parent::go2("/formularios/formulario/$f->nome");
+            parent::go2("/formularios/tela-relatorio/$f->nome");
         }
 
         if ($this->relatorio->checarAcesso($relatorio[0]->id) !== TRUE) { 
