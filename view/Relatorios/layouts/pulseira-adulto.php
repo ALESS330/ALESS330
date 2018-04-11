74dd798ca64f2472e88e0d808d1c6a170aca7faf
marcos <marcos@abd22e6b-81ca-4246-bfda-0ddccaaa1310>
2018-04-11 20:07:45 +0000

    git-svn-id: http://10.18.0.71/svn/relator@48 abd22e6b-81ca-4246-bfda-0ddccaaa1310

--- view/Relatorios/layouts/pulseira-adulto.php
+++ view/Relatorios/layouts/pulseira-adulto.php
@@ -128,7 +128,7 @@ $user = new Usuario($_SESSION['username']);
         <option value="HUGD-PULS-COBS02">Centro Obstétrico - Neonato</option>
         <option value="HUGD-PULS-COBS01">Centro Obstétrico - Adulto</option>
         <option value="HUGD-PULS-RINTER01">Recepção de Internação - Adulto</option>
-        <!--option value="HUGD-PULS-RINTER01">Recepção de Internação - Pediátrico</option -->
+        <option value="HUGD-PULS-RINTER02">Recepção de Internação - Pediátrico</option>
         <option value="HUGD-PULS-PAGO01">Recepção de Maternidade - Adulto</option>
         <!--option value="HUGD-PULS-PAGO01">Recepção de Maternidade - Neonato</option -->        
     </select>
--- view/Relatorios/layouts/pulseira-neonato.php
+++ view/Relatorios/layouts/pulseira-neonato.php
@@ -141,9 +141,9 @@ $user = new Usuario($_SESSION['username']);
         <?php } ?>
         <option value="HUGD-PULS-COBS02">Centro Obstétrico - Neonato</option>
         <option value="HUGD-PULS-COBS01">Centro Obstétrico - Adulto</option>
-        <!--        <option value="HUGD-PULS-RINTER01">Recepção de Internação - Adulto</option>-->
-        <!--option value="HUGD-PULS-RINTER01">Recepção de Internação - Pediátrico</option -->
+        <option value="HUGD-PULS-RINTER01">Recepção de Internação - Adulto</option>
+        <option value="HUGD-PULS-RINTER02">Recepção de Internação - Pediátrico</option>
         <option value="HUGD-PULS-PAGO01">Recepção de Maternidade - Adulto</option>
-        <!--option value="HUGD-PULS-PAGO01">Recepção de Maternidade - Neonato</option -->        
+        <!--option value="HUGD-PULS-PAGO01">Recepção de Maternidade - Neonato</option -->              
     </select>
 </form>
