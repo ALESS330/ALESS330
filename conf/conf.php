57fdd0a9050f056ac6a89bf8e947a04f371a4878
alessandro <alessandro@abd22e6b-81ca-4246-bfda-0ddccaaa1310>
2018-04-24 20:26:16 +0000

    git-svn-id: http://10.18.0.71/svn/relator@55 abd22e6b-81ca-4246-bfda-0ddccaaa1310

--- conf/conf.php
+++ conf/conf.php
@@ -8,11 +8,11 @@ $db["password"] = "teste";
 $db["database"] = "teste";
 $db['porta']    = 5432; 
 
-//$db["server"]   = "10.18.0.58";
-//$db["user"]     = "postgres";
-//$db["password"] = "Zxcpoi2k0";
-//$db["database"] = "huufgd_bd";
-//$db['porta']    = 5432; 
+$db["server"]   = "10.18.0.58";
+$db["user"]     = "postgres";
+$db["password"] = "Zxcpoi2k0";
+$db["database"] = "huufgd_bd";
+$db['porta']    = 5432; 
 
 $db["aghu"]["server"]   = "10.18.0.30";
 $db["aghu"]["porta"]    = 6544;
