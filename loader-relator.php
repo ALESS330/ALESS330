9bde8a31a290589b517fc1302d1f3bbbf1261cc8
alessandro <alessandro@abd22e6b-81ca-4246-bfda-0ddccaaa1310>
2019-03-06 18:39:40 +0000

    git-svn-id: http://10.18.0.71/svn/relator@73 abd22e6b-81ca-4246-bfda-0ddccaaa1310

--- loader-relator.php
+++ loader-relator.php
@@ -1,7 +1,7 @@
 <?php
 
-$corSistema = " blue-grey darken-4 ";
-$corSistemaRGB = "#263238";
+$corSistema = " blue-grey darken-3 ";
+$corSistemaRGB = "#37474f";
 $corBasicaSistema = " blue-grey ";
 $nomeSistema = 'relator';
 $tituloSistema = 'Relatórios';
@@ -9,5 +9,6 @@ $descricaoSistema = 'Sistema de Emissão de Relatórios';
 $sisdir = "relator";
 $contexto = "/$sisdir";
 $iconeSistema = "/$sisdir/assets/img/$nomeSistema.png";
+$faviconSistema = "/$sisdir/assets/img/$nomeSistema-favicon.png";
  
 require_once dirname(dirname(__FILE__))."/base-z/preindex.php";
