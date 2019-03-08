b5f8e9dc5b97f2fcfca596b2394f8ae61a46fc53
alessandro <alessandro@abd22e6b-81ca-4246-bfda-0ddccaaa1310>
2019-03-08 20:35:21 +0000

    git-svn-id: http://10.18.0.71/svn/relator@76 abd22e6b-81ca-4246-bfda-0ddccaaa1310

--- loader-relator.php
+++ loader-relator.php
@@ -9,6 +9,6 @@ $descricaoSistema = 'Sistema de Emissão de Relatórios';
 $sisdir = "relator";
 $contexto = "/$sisdir";
 $iconeSistema = "/$sisdir/assets/img/$nomeSistema.png";
-$faviconSistema = "/$sisdir/assets/img/$nomeSistema-favicon.png";
- 
-require_once dirname(dirname(__FILE__))."/base-z/preindex.php";
+$faviconSistema = "/$sisdir/assets/img/$nomeSistema";
+
+require_once dirname(dirname(__FILE__)) . "/base-z/preindex.php";
