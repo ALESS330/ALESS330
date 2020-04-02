b3901148fc10086c5b5c724844e626304218bc60
marcos <marcos@abd22e6b-81ca-4246-bfda-0ddccaaa1310>
2020-04-02 13:56:58 +0000

    git-svn-id: http://10.18.0.71/svn/relator@117 abd22e6b-81ca-4246-bfda-0ddccaaa1310

--- view/Relatorios/layouts/rotulo-medicamento.php
+++ view/Relatorios/layouts/rotulo-medicamento.php
@@ -1,73 +1,112 @@
-<?php
-global $orientacao, $margem;
-$margem = array(
-    'margin_left' => 0, 
-    'margin_right' => 0, 
-    'margin_top' => 0, 
-    'margin_bottom' => 0, 
-    'margin_header' => 0, 
-    'margin_footer' => 0);
-$orientacao = "L";
-$paciente = $dados[0];
-?>
-<style type="text/css">
+#{botoes}
+<div class="fixed-action-btn no-print">
+    <button class="btn-floating btn-large red" id="bt-imprimir" title="Imprimir" onclick="window.print();">
+        <i class="material-icons">local_printshop</i>
+    </button>
+</div>
+#{/botoes}
 
-    .rotulo{
-        width: 63mm;
-        height: 100mm;
-        background-image: url('/relator/assets/img/fundo-rotulo-medicamento.jpg');
-        background-size: 98%;
-        background-position: center;
-        background-repeat: no-repeat;
-        padding-top: 5mm;
-        padding-left: 5mm;
-        padding-right: 5mm;
-        padding-bottom: 0mm;
-        float: left;
-/*        border: solid 1px silver;*/
-        font-size: 10pt;
-        font-family: Arial;
+<?php 
+    $paciente = $dados[0];
+?>
+<style>
+    body {
+        font-family: 'Google Sans' !important;
     }
 
-    .rotulo ul li{
-        list-style: none;
+    .conteudo {
+        width: 297mm;
+        height: 210mm;
+        margin: 0 auto;
+        margin-top: 15px;
+        box-shadow: 0 0.5px 3px 0.5px grey;
+        padding: 10mm;
+        margin-bottom: 5px;
+    }
+
+    .rotulo{
+        width: 66mm;
+        height: 94mm;
+        background-image: url('/relator/assets/img/fundo-rotulo-medicamento.jpg');
+        background-size: 100%;
+        background-position: 0 0;
+        background-repeat: no-repeat;
+        margin: 1mm;
+        float: left;
+        font-size: 9pt;
+        font-family: Arial;
+        border: solid 1px silver;
     }
 
     .rotulo .titulo-rotulo{
-        padding-left: 30mm;
-        font-weight: bold;
-        font-size: 8pt;
+        padding-left: 32mm !important;
+        font-weight: bold !important;
+        font-size: 7pt !important;
         color: white;
-        padding-bottom: 2mm;
+        padding-bottom: 2mm !important;
+        padding-top: 6px !important;
     }
     strong{
         font-size: 8pt;
         line-height: 10pt;
     }
     .medicamento{
-        height: 40mm;
+        height: 50mm;
     }
-    div.dados div{
-        line-height: 7mm;
+
+    .rotulo div{ 
+        margin-left: 1mm;
+    }
+
+    @media print {
+        @page { 
+            margin: 0 !important;
+        }
+        body {
+            font-family: Roboto !important;
+            /* margin-top: 1mm;
+            margin-bottom: 5mm; */
+        }
+        .conteudo {
+            box-shadow: none;
+            margin-top: 0px;
+            padding-top: 2.5mm !important;
+            height: 200mm;
+        }
+        .no-print {
+            display: none;
+        }
     }
 </style>
+
+<div class="conteudo">
 <?php for ($i = 0; $i <= 7; $i++) { ?>
     <div class="rotulo">
         <div class="titulo-rotulo">Rótulo de Medicação</div>
         <div class="paciente">
-            <strong>Paciente: </strong><span><?= $paciente['nome'] ?></span>
+            <div class="nome-paciente"><strong><?= $paciente['nome'] ?></strong></div>
             <strong>DN: </strong><span><?= $paciente['data_nascimento'] ?></span>        
         </div>
         <div class="medicamento">
-            <strong>Diluente: </strong> _________ <strong>Volume: </strong> ___________
+            <div>
+                <strong>Diluente: </strong> _________ <strong>
+            </div>
+            <div>
+                Volume: </strong> ___________
+            </div>
             <strong>Medicamento/dose: </strong>
         </div>
         <div class="dados">
+            
             <div><strong>Tempo de infusão:</strong> _______h</div>
-            <div><strong>Velocidade: </strong> _____ <small>gts/min</small> ou _____ <small>ml/h</small></div>
-            <div><strong>Hora Início:</strong> ___:___ <strong>Hora Término: </strong> ___:___ </div>
+            <div><strong>Velocidade: </strong> _____ <small>gts/min ou _____ ml/h</small></div>
+            <div>
+                <strong>Hora Início:</strong> ___:___
+                <strong>Término</strong> ___:___ 
+            </div>
             <div><strong>Data: </strong> ____/____/________</div>
             <div><strong>Profissional: </strong>__________________</div>
         </div>
     </div>
 <?php } ?>
+</div>
