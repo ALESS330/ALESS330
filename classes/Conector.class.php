36b959d95222e0de3257fa47ef031ca70dd03a03
aless330 <alessandro.teixeirandrade@gmail.com>
2021-09-21 12:00:24 -0400

    Layout do termo de responsabilidade

--- classes/Conector.class.php
+++ classes/Conector.class.php
@@ -5,12 +5,14 @@
  *
  * @author marcos
  */
-class Conector {
+class Conector
+{
 
     private $conexoes;
     private $erros;
 
-    function __construct() {
+    function __construct()
+    {
         global $db;
         $this->conexoes["relatorios"] = new PDO("pgsql:dbname=" . $db['database'] . ";user=" . $db['user'] . ";password=" . $db['password'] . ";host=" . $db['server']);
         $t = $this->conexoes["relatorios"]->setAttribute(PDO::ATTR_PERSISTENT, true);
@@ -47,53 +49,58 @@ class Conector {
         }
     }
 
-    function getConexaoAGHU() {
+    function getConexaoAGHU()
+    {
         return $this->conexoes["aghu"];
     }
 
-    function getConexaoSistema() {
+    function getConexaoSistema()
+    {
         return $this->conexoes["relatorios"];
     }
 
-    function getDados($sql, $conex) {
-        try { 
+    function getDados($sql, $conex)
+    {
+        try {
             $r = $conex->prepare($sql);
             $result = $r->execute();
             $linhas = $r->fetchAll(PDO::FETCH_ASSOC);
-            echo "<pre>"; print_r($linhas); exit(0);
+
             //se não veio nenhuma linha, é preciso extrair os nomes das colunas de outra maneira
-            if(count($linhas)==0){
+            if (count($linhas) == 0) {
                 //descobrir quantas colunas
                 $nCols = $r->columnCount();
                 $vazio = array();
                 //se nenhum, é pq teve erro do banco de dados (falta campo, conexão etc)
-                if($nCols == 0){
+                if ($nCols == 0) {
                     throw new Exception("Erro na busca dos campos");
                 }
                 //iterando pelas colunas existentes ...
-                foreach(range(0, $nCols -1 ) as $column_index ) 
-                { 
-                  $columnMeta = $r->getColumnMeta($column_index)['name'];
-                  //... e construindo um array para isso
-                  $vazio[0][$columnMeta] = null;
+                foreach (range(0, $nCols - 1) as $column_index) {
+                    $columnMeta = $r->getColumnMeta($column_index)['name'];
+                    //... e construindo um array para isso
+                    $vazio[0][$columnMeta] = null;
                 }
                 return $vazio;
             }
         } catch (Exception $ex) {
-            die("Conexão não efetuada $conex. (". $ex->getMessage.")");
+            die("Conexão não efetuada $conex. (" . $ex->getMessage . ")");
         }
-        return $linhas; 
+        return $linhas;
     }
 
-    function getDadosSistema($sql) {
+    function getDadosSistema($sql)
+    {
         return $this->getDados($sql, $this->conexoes["relatorios"]);
     }
 
-    function getDadosAGHU($sql) {
+    function getDadosAGHU($sql)
+    {
         return $this->getDados($sql, $this->conexoes["aghu"]);
     }
 
-    function getConexao($nome) {
+    function getConexao($nome)
+    {
         if (isset($this->conexoes[$nome])) {
             return $this->conexoes[$nome];
         }
@@ -101,5 +108,4 @@ class Conector {
         $datasource = $oDatasource->selectBy(array("nome" => $nome))[0];
         throw new Exception("Conexão inexistente ou inativa para '$datasource->descricao' ($nome)");
     }
-
 }
